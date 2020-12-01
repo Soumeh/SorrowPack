8e454da223411cd3ba3517b822c45d43c9b3bc8e
Soumeh <stefi.brezovac@gmail.com>
2020-12-01 17:18:49 +0100

    "init" because yeah

--- .github/workflows/generateSha1.py
+++ .github/workflows/generateSha1.py
@@ -1,6 +1,20 @@
-import os, hashlib, jsonBLOCKSIZE = 65536def getSha1(item):    hasher = hashlib.sha1()    with open(item, 'rb') as thisFile:        buf = thisFile.read(BLOCKSIZE)        while len(buf) > 0:            hasher.update(buf)            buf = thisFile.read(BLOCKSIZE)    return hasher.hexdigest()def makeJson(Sha1):    os.chdir('build')
-    if 'Sha1.json' in os.listdir():
-        index = 0
-        while 'Sha1_{}.json'.format(index) in os.listdir():
-            index += 1
-        os.rename('Sha1.json', 'Sha1_{}.json'.format(index))    with open('Sha1.json', 'w') as thisFile:        thisDict = {'Sha1': Sha1}        json.dump(thisDict, thisFile)Sha1 = getSha1('build/SorrowPack.zip')makeJson(Sha1)
+import os, hashlib, json
+BLOCKSIZE = 65536
+
+def getSha1(item):
+    hasher = hashlib.sha1()
+    with open(item, 'rb') as thisFile:
+        buf = thisFile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = thisFile.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
+def makeJson(Sha1):
+    os.chdir('build')
+    with open('Sha1.json', 'w+') as thisFile:
+        thisDict = {'Sha1': Sha1}
+        json.dump(thisDict, thisFile)
+
+Sha1 = getSha1('build/SorrowPack.zip')
+makeJson(Sha1)
